66d4406a8cb7f5d20f446fe0a4368de09463feee
EvanilsonP <pevanilson99@gmail.com>
2023-03-10 09:25:01 -0300

    connecting to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { connectToDb, getDb } = require('./db');
 const app = express();
+
+// db connection // listening to requests after we are connected for the database
+let db;
+connectToDb((err) => {
+    if(!err) {
+        app.listen(3000, () => { console.log('Listening on port 3000.')});
+    }
+    db = getDb();
+});
 
 // routes
 app.get('/books', (req, res) => {
     res.json({ msg: "Welcome to the API." });
 });
-
-app.listen(3000, () => { console.log('Listening on port 3000.')});
